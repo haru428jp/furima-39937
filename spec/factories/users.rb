76875940bfd64b05d7916b74d78a28c027f51044
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-11 14:33:06 +0900

    Gimeiの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}}
+    email                 {Faker::Internet.email}
     password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {'山田'}
-    first_name            {'太郎'}
-    last_name_kana        {'ヤマダ'}
-    first_name_kana       {'タロウ'}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
     birthday              {Faker::Date.birthday}
   end
 end
