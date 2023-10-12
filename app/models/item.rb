07649068427ca40282d0c1612cfd481c4dea3c57
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-12 18:20:47 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_time
 end
