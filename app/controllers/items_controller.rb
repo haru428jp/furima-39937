94c16707c5e81c0b7ac5049018e4ded7a07a4b66
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-14 15:39:40 +0900

    Rubocopの実行とcheck内容の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,10 +36,10 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
-      item.destroy
-      redirect_to root_path
-    end
+    return unless user_signed_in? && current_user.id == item.user_id
+
+    item.destroy
+    redirect_to root_path
   end
 
   private
