cb9d33d5a38854ccfb4a386b3d8d418970d585a2
haru428jp <146315449+haru428jp@users.noreply.github.com>
2023-10-16 22:10:28 +0900

    ファクトリーとテストコードを修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    item_id { Faker::Number.non_zero_digit }
-    user_id { Faker::Number.non_zero_digit }
     zip_code      { Faker::Number.binary(digits: 3) + '-' + Faker::Number.binary(digits: 4) }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city          { Faker::Address.city }
